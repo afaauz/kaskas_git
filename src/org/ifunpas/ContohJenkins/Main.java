1e7633b644b8abdc4d5581d8e2863acb6a73089e
SB604-14 <SB604-14@DESKTOP-P25BHNP>
2019-11-25 14:01:32 +0700

    practice 0.1

--- src/org/ifunpas/ContohJenkins/Main.java
+++ src/org/ifunpas/ContohJenkins/Main.java
@@ -3,5 +3,7 @@ package org.ifunpas.ContohJenkins;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello jenkins!");
+		
+		System.out.println("practice 0.1");
 	}
 }
