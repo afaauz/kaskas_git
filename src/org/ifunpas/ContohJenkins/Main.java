21c317ee9714617175526141b3dd050fd0dddda9
SB604-14 <SB604-14@DESKTOP-P25BHNP>
2019-11-25 14:05:33 +0700

    static void test;

--- src/org/ifunpas/ContohJenkins/Main.java
+++ src/org/ifunpas/ContohJenkins/Main.java
@@ -4,6 +4,13 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello jenkins!");
 		
-		System.out.println("practice 0.1");
+		System.out.println("practice 0.2");
+		
+		test();
+	}
+	
+	static void test() {
+		String tester = "xa";
+		System.out.println(tester);
 	}
 }
